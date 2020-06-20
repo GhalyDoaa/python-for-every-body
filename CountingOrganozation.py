3f65bc2945267388f6e612dc80ede8bc989d8243
Doaa Ghaly <doaaghaly397@gmail.com>
2020-06-20 18:48:33 -0400

    Add files via upload

---
+++ CountingOrganozation.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+conn = sqlite3.connect('email.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('''
+CREATE TABLE Counts (email TEXT, count INTEGER)''')
+
+fname = input('Enter file name: ')
+if (len(fname) < 1): fname = 'mbox-short.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From: '): continue
+    pieces = line.split('@')[1]
+    index = pieces.find('@') + 1
+    org = pieces[index:]
+    print("org is ", org)
+    cur.execute('SELECT count FROM Counts WHERE email = ? ', (org,))
+    row = cur.fetchone()
+    # fetchone() => This method retrieves the next row of a query result set and returns a single sequence,
+    # or None if no more rows are available.
+    if row is None:
+        cur.execute('''INSERT INTO Counts (email, count)
+                VALUES (?, 1)''', (org,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?',
+                    (org,))
+    conn.commit()
+
+# https://www.sqlite.org/lang_select.html
+print("select all from db")
+sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC '
+
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
+
